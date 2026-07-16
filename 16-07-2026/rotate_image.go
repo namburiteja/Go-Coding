db90fbe094f73bd31e45ea5c350e81a3df1aec90
namburiteja <tejanamburi813@gmail.com>
2026-07-16 06:15:48 +0000

    2nd code

---
+++ 16-07-2026/rotate_image.go
@@ -0,0 +1,14 @@
+func rotate(matrix [][]int)  {
+    m := len(matrix)
+    n := len(matrix[0])
+    for i:=0;i<m;i++ {
+        for j:=i+1;j<n;j++ {
+            matrix[i][j] , matrix[j][i] = matrix[j][i] , matrix[i][j]
+        }
+    }
+    for i:=0;i<m;i++ {
+        for j:=0;j<n/2;j++ {
+            matrix[i][j] , matrix[i][n-j-1] = matrix[i][n-j-1] , matrix[i][j]
+        }
+    }
+}
